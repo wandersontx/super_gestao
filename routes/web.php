cc9909b888a620ea72ea4f7ea034867df8e4597c
wandersontx <wandersonh89@gmail.com>
2020-12-29 15:26:33 -0300

    rotas basicas

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
-
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-Route::get('/contato', 'ContatoController@contato');
-
-Route::get('/login', function(){return 'login';});
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'login';})->name('site.login');
 
 Route::prefix('app')->group(function(){
-    Route::get('/clientes', function(){return 'clientes';});
-    Route::get('/fornecedores', function(){return 'fornecedores';});
-    Route::get('/produtos', function(){return 'produtos';});
+    Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. clique <a href="'.route('site.index').'">aqui</a> para retornar para página principal.';
 });
