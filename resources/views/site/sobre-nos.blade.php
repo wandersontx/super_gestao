8d95eca8112ab77f3a981e2783780bcfbb49cf71
wandersontx <wandersonh89@gmail.com>
2020-12-30 23:08:13 -0300

    @extends, @yield, @section

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">   
-    </head>
-
-    <body>
-        <div class="topo">
-
-            <div class="logo">
-                <img src="{{ asset('img/logo.png') }}">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
-        </div>
-
+@extends('site.layouts.basico')
+@section('titulo','Sobre nós')
+@section('content')
         <div class="conteudo-pagina">
             <div class="titulo-pagina">
                 <h1>Olá, eu sou o Super Gestão</h1>
@@ -51,5 +30,4 @@
                 <img src="{{ asset('img/mapa.png') }}">
             </div>
         </div>
-    </body>
-</html>
+@endsection
