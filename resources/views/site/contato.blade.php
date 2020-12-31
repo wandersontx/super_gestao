9f22b1d943e6ac244cc0719765d63a4f2ecab999
wandersontx <wandersonh89@gmail.com>
2020-12-30 23:36:28 -0300

    Csrf

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -8,21 +8,22 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            <form>
-                <input type="text" placeholder="Nome" class="borda-preta">
+            <form  action="{{ route('site.contato') }}" method="post">
+                @csrf
+                <input type="text" placeholder="Nome" name="nome" class="borda-preta">
                 <br>
-                <input type="text" placeholder="Telefone" class="borda-preta">
+                <input type="text" placeholder="Telefone" name="telefone" class="borda-preta">
                 <br>
-                <input type="text" placeholder="E-mail" class="borda-preta">
+                <input type="text" placeholder="E-mail" name="email" class="borda-preta">
                 <br>
-                <select class="borda-preta">
+                <select class="borda-preta" name="motivo_contato">
                     <option value="">Qual o motivo do contato?</option>
-                    <option value="">Dúvida</option>
-                    <option value="">Elogio</option>
-                    <option value="">Reclamação</option>
+                    <option value="duvida">Dúvida</option>
+                    <option value="elogio">Elogio</option>
+                    <option value="reclamacao">Reclamação</option>
                 </select>
                 <br>
-                <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
+                <textarea class="borda-preta" name="messagem" >Preencha aqui a sua mensagem</textarea>
                 <br>
                 <button type="submit" class="borda-preta">ENVIAR</button>
             </form>
