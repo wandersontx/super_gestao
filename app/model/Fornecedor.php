53392525e1b3a1d559fcdfa4dba634e7bea889d5
wandersontx <wandersonh89@gmail.com>
2021-01-09 00:30:22 -0300

    forma de sobrescrever a nomeação automatica do eloquent

--- app/model/Fornecedor.php
+++ app/model/Fornecedor.php
@@ -5,6 +5,7 @@ namespace App\model;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
-{
-    //
+{   
+    //forma de sobrescrever a nomeação automatica do eloquent
+    protected $table = 'fornecedores';
 }
