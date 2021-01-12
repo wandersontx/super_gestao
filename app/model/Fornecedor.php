b4cecb6687d93913e114ee52ebf15f9136965bfb
wandersontx <wandersonh89@gmail.com>
2021-01-11 23:28:08 -0300

    fillable

--- app/model/Fornecedor.php
+++ app/model/Fornecedor.php
@@ -8,4 +8,7 @@ class Fornecedor extends Model
 {   
     //forma de sobrescrever a nomeação automatica do eloquent
     protected $table = 'fornecedores';
+    //defini os atributos que poderam ser salvos no banco de dados
+    //atraves do metodo statico create.
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
