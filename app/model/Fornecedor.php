9585bf8d9ab2e352f97cf40441aed6faed19b297
wandersontx <wandersonh89@gmail.com>
2021-01-13 21:08:31 -0300

    implementaçao softdelete

--- app/model/Fornecedor.php
+++ app/model/Fornecedor.php
@@ -3,9 +3,13 @@
 namespace App\model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {   
+    //Utilizando o recurso de trait para implementar softdeletes
+    use SoftDeletes;
+
     //forma de sobrescrever a nomeação automatica do eloquent
     protected $table = 'fornecedores';
     //defini os atributos que poderam ser salvos no banco de dados
