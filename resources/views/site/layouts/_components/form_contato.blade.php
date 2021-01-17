fb22549c81ab0f5867e201e878e4c117ccd0b0bd
wandersontx <wandersonh89@gmail.com>
2021-01-16 23:17:56 -0300

    metodo old no formulario

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -6,11 +6,11 @@
 
 <form  action="{{ route('site.contato') }}" method="post">
     @csrf
-    <input type="text" placeholder="Nome" name="nome" class="{{ $classe }}">
+    <input type="text" placeholder="Nome" name="nome" class="{{ $classe }}" value="{{ old('nome') }}">
     <br>
-    <input type="text" placeholder="Telefone" name="telefone" class="{{ $classe }}">
+    <input type="text" placeholder="Telefone" name="telefone" class="{{ $classe }}" value="{{ old('telefone') }}">
     <br>
-    <input type="text" placeholder="E-mail" name="email" class="{{ $classe }}">
+    <input type="text" placeholder="E-mail" name="email" class="{{ $classe }}" value="{{ old('email') }}">
     <br>
     <select class="{{ $classe }}" name="motivo_contato">
         <option value="">Qual o motivo do contato?</option>
@@ -19,7 +19,7 @@
         <option value="3">Reclamação</option>
     </select>
     <br>
-    <textarea class="{{ $classe }}" name="mensagem" >Preencha aqui a sua mensagem</textarea>
+    <textarea class="{{ $classe }}" name="mensagem">{{ !empty(old('mensagem')) ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
