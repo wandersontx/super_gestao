4f5ff70c15c33d6fa8a47cdf51d1a5eadd35051e
wandersontx <wandersonh89@gmail.com>
2021-03-13 00:50:14 -0300

    Exibindo um produto em especifico

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -63,7 +63,7 @@ class ProdutoController extends Controller
      */
     public function show(Produto $produto)
     {
-        //
+        return view('app.produto.show', ['produto' => $produto]);
     }
 
     /**
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -24,6 +24,7 @@
                              <th>Unidade ID</th>
                              <th>#</th>
                              <th>#</th>
+                             <th>#</th>
                          </tr>
                      </thead>
                      <tbody>
@@ -33,6 +34,7 @@
                                 <td>{{ $item->descricao }}</td>
                                 <td>{{ $item->peso }}</td>
                                 <td>{{ $item->unidade_id }}</td>
+                                <td><a href="{{ route('produto.show', ['produto' => $item->id ]) }}">visualizar</a></td>
                                 <td><a href="#">Editar</a></td>
                                 <td><a href="#">Excluir</a></td>
                             </tr>
